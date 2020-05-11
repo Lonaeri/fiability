1c995704fb31882c5c4a2d0a04b15a34e1b18d90
Lonaeri <lorianedehu@gmail.com>
2020-05-11 10:06:53 +0200

    Titres et format

--- simulationController.py
+++ simulationController.py
@@ -1,11 +1,22 @@
 import numpy as np
 from functools import partial
-from typing import Callable
+from typing import Callable, List
+import matplotlib.pyplot as plt
 
 NB_MAX_STEPS = 100
 NB_MIN_STEPS = 10
 
-def stepRecommendation(duration):
+class Simulation:
+    def __init__(self, nb_systems, duration, cadence, breakdowns, real_mtff, breakdowns_times, nb_steps):
+        self.nb_systems = nb_systems
+        self.duration = duration
+        self.cadence = cadence
+        self.breakdowns = breakdowns
+        self.breakdowns_times = breakdowns_times
+        self.real_mtff = real_mtff
+        self.nb_steps = nb_steps
+
+def step_recommendation(duration):
     if (duration < NB_MIN_STEPS):
         return duration
 
@@ -16,43 +27,84 @@ def stepRecommendation(duration):
         counter = NB_MAX_STEPS
     return counter
 
-def calculSimulations(nbSystems, duration, probability, nbSteps):
-    if (nbSystems <= 0):
+def calcul_simulations(nb_systems, duration, probability, nb_steps, beta):
+    if (nb_systems <= 0):
         return (1, [])
     if (duration <= 0):
         return (2, [])
-    if (nbSteps <= 0):
+    if (nb_steps <= 0):
+        return (4, [])
+    if (beta <= 0):
         return (4, [])
 
-    probaType, probaValue = probability
-    if (probaType == 2):
-        cadence = 1 / probaValue
+    proba_type, proba_value = probability
+    if (proba_type == 2):
+        cadence = 1 / proba_value
     else:
-        cadence = probaValue
+        cadence = proba_value
     if (cadence <= 0 or cadence > 1):
         return (3, [])
 
-    nb_remains = []
-    nb_remains.append((nbSystems, 1))
-    remains_before = nbSystems
-    for i in range(1, nbSteps):
-        nb_breakdowns, reliability = simu_expo(remains_before, cadence, (duration / nbSteps) * i)
-        remains_before = remains_before - nb_breakdowns
-        nb_remains.append((remains_before, reliability))
-    return (0, nb_remains)
+    # nb_remains = []
+    # nb_remains.append((nb_systems, 1))
+    # remains_before = nb_systems
+    # for i in range(1, nb_steps):
+    #     nb_breakdowns, reliability = simu_expo(remains_before, cadence, (duration / nb_steps) * i)
+    #     remains_before = remains_before - nb_breakdowns
+    #     nb_remains.append((remains_before, reliability))
+    uniform_values = np.random.uniform(size = nb_systems)
+    values = generate_weibull_law(cadence, beta, uniform_values)
+    nb_breakdowns = get_nb_element_by_step(duration, nb_steps, values)
+    real_mtff = np.mean(values)
 
+    # plt.ion()
+    plt.plot(np.sort(values), range(nb_systems, 0, -1))
+    plt.title('Courbe de survie v(t)')
+    plt.ylabel('Nb Systèmes restants')
+    plt.xlabel('Temps (defaut : heure)')
+    plt.axvline(x=duration, color='gray', linestyle='--')
+    print(weibull_law(cadence, beta, np.sort(values), nb_systems))
+    plt.plot(np.sort(values), weibull_law(cadence, beta, np.sort(values), nb_systems))
+    plt.show(block=False)
 
-def simu_expo(nb_systems: int, cadence: float, time: float):
-    return breakdowns_simulation(partial(expo_survival_law, cadence), nb_systems, time)
+    current_simulation = Simulation(nb_systems, duration, cadence, nb_breakdowns, real_mtff, values, nb_steps)
+    return (0, current_simulation)
 
+def weibull_law(alpha, beta, times, nb_systems):
+    return np.exp(-((times * alpha) ** beta))* nb_systems
 
-def expo_survival_law(alpha: float, t: float)-> float:
-    return np.exp(-alpha * t)
+# def simu_expo(nb_systems: int, cadence: float, time: float):
+#     return breakdowns_simulation(partial(expo_survival_law, cadence), nb_systems, time)
 
+# def expo_survival_law(alpha: float, t: float)-> float:
+#     return np.exp(-alpha * t)
 
-def breakdowns_simulation(survival_law: Callable[[float], float], N: int, T: float):
-    uniform_values = np.random.uniform(size=N)
-    reliability = survival_law(T)
+def get_nb_element_by_step(duration, nb_steps, values):
+    steps = [0] * nb_steps
+    for value in values:
+        index = int(value // (duration / (nb_steps) ))
+        if (index >= nb_steps):
+            continue
+        steps[index ] += 1
 
-    breakdowns = uniform_values[uniform_values < 1 - reliability]
-    return len(breakdowns), reliability
+    for index in range(1, nb_steps):
+        steps[index] += steps[index - 1]
+    return steps
+
+# def use_expo_survival_law(alpha : float, uniform_values : float):
+#     return generate_weibull_law(alpha, 1, uniform_values)
+
+# def weibull_survival_law(alpha : float, time : float, beta : float):
+#     return np.exp(-(alpha * time) ** beta)
+
+def generate_weibull_law(alpha: float, beta: float, uniform_values: List[float]) -> List[float]:
+    a = -np.log(1 - uniform_values) / np.power(alpha, beta)
+    b = 1 / beta
+    return a ** b
+
+# def breakdowns_simulation(survival_law: Callable[[float], float], N: int, T: float):
+#     uniform_values = np.random.uniform(size=N)
+#     reliability = survival_law(T)
+
+#     breakdowns = uniform_values[uniform_values < 1 - reliability]
+#     return len(breakdowns), reliability
--- view.py
+++ view.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from typing import List
 from simulationController import *
+import tkinter.font as tkFont
 
 class View(tk.Frame):
     def __init__(self, master=None):
@@ -13,8 +14,13 @@ class View(tk.Frame):
     def create_widgets(self):
         self.systems_view = []
         self.stepsFrame = []
+        
+        self.title = tk.Label(self, 
+            text="Equipement de Vaisseau Spatial",
+            font = tkFont.Font(size=16, weight="bold", underline=1))
+        self.title.pack(side="top")
 
-        self.description = tk.Label(self, text="Attention : Tous les durées doivent être dans la même unité")
+        self.description = tk.Label(self,pady=5,font=tkFont.Font(size=10, slant='italic'), text="Attention : Tous les durées doivent être dans la même unité")
         self.description.pack(side="top")
 
         errors_frame = tk.Frame(self)
@@ -22,20 +28,15 @@ class View(tk.Frame):
         errors_frame.pack()
 
         # Nb of systems
-        self.frame_nb_simulation = tk.Frame(master=self)
+        self.frame_nb_simulation = tk.Frame(master=self, pady=7)
         tk.Label(self.frame_nb_simulation, text="Nombre de simulations").pack(side = tk.LEFT)
         tk.Entry(self.frame_nb_simulation, name="nbSystems").pack(side = tk.LEFT)
         self.frame_nb_simulation.pack()
-
-        # Duration of the simulation
-        self.frame_duration = tk.Frame(master=self)
-        tk.Label(self.frame_duration, text="Durée de la simulation").pack(side = tk.LEFT)
-        tk.Entry(self.frame_duration, name="duration", validatecommand=self.changeRecommendation, validate="focusout").pack(side = tk.LEFT)
-        self.frame_duration.pack()
         
         # MTFF : Mean time to first failure 
         # λ : number of expected occurrences
-        radios = tk.Frame(master=self)
+        group_radio = tk.LabelFrame(master=self, pady=7, padx=7)
+        radios = tk.Frame(master=group_radio)
         self.radio_value = tk.IntVar()
         radio_lambda = tk.Radiobutton(radios, text="Lambda", cursor="hand2", variable=self.radio_value, value=1, command=self.change_option)
         radio_lambda.select()
@@ -43,31 +44,51 @@ class View(tk.Frame):
         tk.Radiobutton(radios, text="mtff", cursor="hand2", variable=self.radio_value, value=2, command=self.change_option).pack(side = tk.LEFT)
         radios.pack()
 
-        self.frame_lambda_mtff = tk.Frame(master=self)
+        self.frame_lambda_mtff = tk.Frame(master=group_radio)
         tk.Label(self.frame_lambda_mtff, text="Cadence des pannes", name="lambdaMtffLabel").pack(side = tk.LEFT)
         tk.Entry(self.frame_lambda_mtff, name="probability").pack(side = tk.LEFT)
         self.frame_lambda_mtff.pack()
+        group_radio.pack()
 
-        self.frame_step = tk.Frame(master=self)
+
+        self.frame_beta = tk.Frame(master=self, pady=11)
+        tk.Label(self.frame_beta, text="Beta (default : 1)").pack(side = tk.LEFT)
+        beta = tk.Entry(self.frame_beta, name="beta")
+        beta.pack(side = tk.LEFT)
+        beta.insert(tk.INSERT, "1")
+        self.frame_beta.pack()
+
+        self.frame_step_group = tk.LabelFrame(master=self, text="Pas à pas")
+        # Duration of the simulation
+        self.frame_duration = tk.Frame(master=self.frame_step_group)
+        tk.Label(self.frame_duration, text="Durée de la simulation").pack(side = tk.LEFT)
+        tk.Entry(self.frame_duration, name="duration", validatecommand=self.change_recommendation, validate="focusout").pack(side = tk.LEFT)
+        self.frame_duration.pack()
+
+        self.frame_step = tk.Frame(master=self.frame_step_group)
         tk.Label(self.frame_step, text="Nombre d'etapes de la simulation").pack(side = tk.LEFT)
-        tk.Spinbox(self.frame_step, from_=1, to=NB_MAX_STEPS, name="nbSteps").pack(side = tk.LEFT)
-        tk.Label(self.frame_step, text="Entre 1 et " + str(NB_MAX_STEPS) + " étapes", name="recommendation").pack(side=tk.BOTTOM)
+        tk.Spinbox(self.frame_step, from_=1, to=NB_MAX_STEPS, name="nb_steps").pack(side = tk.LEFT)
         self.frame_step.pack()
+        tk.Label(self.frame_step_group, text="Entre 1 et " + str(NB_MAX_STEPS) + " étapes", font=tkFont.Font(size=8, slant='italic'), name="recommendation").pack()
+        self.frame_step_group.pack()
 
         self.submit = tk.Button(self, text="Envoyer", command=self.submitForm)
         self.submit.pack()
+        
 
         self.simu = tk.Frame(master=self)
         self.scale = tk.Scale(master=self.simu, orient=tk.HORIZONTAL, from_=1, command=self.go_to_step)
+        self.text_step = tk.Text(master=self.simu, width=50, height=10)
         self.simu.pack()
 
     def submitForm(self):
-        nb_systems = self.getNbSystems()
-        error, self.all_steps = calculSimulations(
+        nb_systems = self.get_nb_systems()
+        error, self.cur_simulation = calcul_simulations(
             nb_systems,
-            self.getDuration(),
-            self.getProbability(),
-            self.getNbSteps()
+            self.get_duration(),
+            self.get_probability(),
+            self.get_nb_steps(),
+            self.get_beta()
         )
         if (error != 0):
             self.errors.configure(text="Format des données entrées invalides")
@@ -76,8 +97,9 @@ class View(tk.Frame):
 
         self.errors.pack_forget()
 
-        self.generate_steps_cursor(self.getNbSteps())
+        self.generate_steps_cursor(self.get_nb_steps())
         self.generate_first_step(nb_systems)
+        self.go_to_step(1)
 
     def change_option(self):
         if (self.radio_value.get() == 1) :
@@ -85,21 +107,22 @@ class View(tk.Frame):
         else :
             self.frame_lambda_mtff.children['lambdaMtffLabel'].configure(text="Moyenne avant 1ere panne")
 
-    def changeRecommendation(self):
-        duration = self.getDuration()
+    def change_recommendation(self):
+        duration = self.get_duration()
         if (duration <= 0):
             return False
-        recommendationNb = stepRecommendation(duration)
-        self.frame_step.children['recommendation'].configure(text="Entre 1 et " + str(NB_MAX_STEPS) + " étapes. Recommandé : " + str(recommendationNb))
+        nb_recommendation = step_recommendation(duration)
+        self.frame_step_group.children['recommendation'].configure(text="Entre 1 et " + str(NB_MAX_STEPS) + " étapes. Recommandé : " + str(nb_recommendation))
         return True
 
     ##############
     # Draw Steps #
     ##############
-    def generate_steps_cursor(self, nbSteps):
-        if (self.scale):
-            self.scale.configure(to=nbSteps)
+    def generate_steps_cursor(self, nb_steps):
+        self.scale.configure(to=nb_steps)
+        if (False == self.scale.winfo_ismapped()):
             self.scale.pack(side=tk.TOP)
+            self.text_step.pack(side=tk.BOTTOM)
 
     def clean_steps(self):
         self.step_frame.destroy()
@@ -117,39 +140,51 @@ class View(tk.Frame):
         simu_view_frame = tk.Frame(self.step_frame)
         self.systems_view = []
         for index in range(0, nb_systems):
-            aLabel = tk.Label(simu_view_frame, bg = "#0f0", padx=8, bd=1, relief="solid")
-            aLabel.grid(row=int(index//nb_col), column=int(index%nb_col))
-            self.systems_view.append(aLabel)
+            a_label = tk.Label(simu_view_frame, bg = "#0f0", padx=8, bd=1, relief="solid")
+            a_label.grid(row=int(index//nb_col), column=int(index%nb_col))
+            self.systems_view.append(a_label)
         simu_view_frame.pack()
-        self.step_frame.pack()
+        self.step_frame.pack(side=tk.TOP)
+
+    def change_step_description(self, step):
+        self.text_step.delete("1.0", tk.END)
+        self.text_step.insert(tk.INSERT, f"MTFF réel de la Simu : {self.cur_simulation.real_mtff}\n")
+        self.text_step.insert(tk.INSERT, f"\nEtape #{step+1}\n")
+        self.text_step.insert(tk.INSERT, f"t : {(self.cur_simulation.duration / self.cur_simulation.nb_steps) * (step + 1)}\n")
+        self.text_step.insert(tk.INSERT, f"Nombre de systemes : {self.cur_simulation.nb_systems}\n")
+        self.text_step.insert(tk.INSERT, f"  -> defaillants : {self.cur_simulation.breakdowns[step]}\n")
+        self.text_step.insert(tk.INSERT, f"  -> OK : {self.cur_simulation.nb_systems - self.cur_simulation.breakdowns[step]}\n")
+        self.text_step.insert(tk.INSERT, f"Pannes simulées {round(((self.cur_simulation.breakdowns[step] / self.cur_simulation.nb_systems) * 100), 2)}%\n")
 
     def go_to_step(self, step):
+        step_index = int(step) - 1
         self.step_frame.configure(text=f"Etape #{step}")
-        nb_remains, reliability = self.all_steps[int(step) - 1]
+        breakdowns = self.cur_simulation.breakdowns[step_index]
         for index, system in enumerate(self.systems_view):
-            if (index >= nb_remains):
-                system.configure(bg='#f00')
-            else :
+            if (index >= breakdowns):
                 system.configure(bg='#0f0')
+            else :
+                system.configure(bg='#f00')
+        self.change_step_description(step_index)
 
     ################
     # Data Getters #
     ################
-    def getNbSystems(self) : 
+    def get_nb_systems(self) : 
         try:
             return int(self.frame_nb_simulation.children['nbSystems'].get())
         except ValueError:
             print("NaN : Nombre de systemes à simuler")
             return -1
 
-    def getDuration(self) : 
+    def get_duration(self) : 
         try:
             return int(self.frame_duration.children['duration'].get())
         except ValueError:
             print("NaN : Durée de la simulation")
             return -1
     
-    def getProbability(self):
+    def get_probability(self):
         try:
             return (
                 self.radio_value.get(),
@@ -159,9 +194,16 @@ class View(tk.Frame):
             print("NaN : Probabilité")
             return (-1, -1)
 
-    def getNbSteps(self) : 
+    def get_nb_steps(self) : 
         try:
-            return int(self.frame_step.children['nbSteps'].get())
+            return int(self.frame_step.children['nb_steps'].get())
         except ValueError:
             print("NaN : Nombre d'etapes")
+            return -1
+
+    def get_beta(self) :
+        try:
+            return float(self.frame_beta.children['beta'].get())
+        except ValueError:
+            print("NaN : Beta")
             return -1
